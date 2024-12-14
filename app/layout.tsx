a1e43752553bc801a30213231090a3192af8a254
Samarth Sheth <samarth.sheth.29@gmail.com>
2024-12-14 15:19:00 +0530

    theme toogle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,29 @@
 import type { Metadata } from "next"
 
+import { ThemeProvider } from "@/components/theme-provider"
+
 import "./globals.css"
 
 export const metadata: Metadata = {
   title: "YT Video Downloader",
   description:
-    "Fast, simple, and reliable YouTube video downloads for all your needs.",
+    "Fast, simple, and reliable YouTube video downloads for all your needs."
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>
+        <ThemeProvider
+          enableSystem
+          attribute="class"
+          defaultTheme="light"
+          disableTransitionOnChange
+          storageKey="yt-video-downloader-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
